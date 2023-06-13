427160d8a2c25ff86695eba6d06c767f9c136858
Falcon_109 <benja.fuente123@gmail.com>
2023-06-13 13:45:37 -0400

    creacion de item y de contarlos

--- Especialidad1/Unidad 3/segundo-proyecto/src/components/TodoItem.jsx
+++ Especialidad1/Unidad 3/segundo-proyecto/src/components/TodoItem.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-export function TodoItem({ todo }) {
-  const { id, task } = todo;
+export function TodoItem({ todo , cambiarEstado }) {
+  const { id, task ,completed} = todo;
+  const fncambiarestado = ()  => {
+    cambiarEstado(id)
+  }
   return (
     <li className="list-group-item" key={id}>
+      <input type="checkbox" className="form-checked-input me-2" onChange={fncambiarestado} checked={completed}/>
       {task}
     </li>
   );
--- Especialidad1/Unidad 3/segundo-proyecto/src/components/TodoList.jsx
+++ Especialidad1/Unidad 3/segundo-proyecto/src/components/TodoList.jsx
@@ -1,4 +1,6 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useRef, useState } from "react";
+/* generar id unicas */
+import uuid4 from "uuid4";
 
 /* Importar TodoItem */
 import { TodoItem } from "./TodoItem";
@@ -6,20 +8,76 @@ import { TodoItem } from "./TodoItem";
 /* Exportar TodoList */
 export function TodoList() {
   const [todos, setTodos] = useState([
-    { id: 1, task:'Tarea 1' }, 
-    { id: 2, task:'Tarea 2' }, 
-    { id: 3 , task:'Tarea 3'},
-    { id: 4 ,task:'Tarea 4'}]);
+  ]);
+
+  const tarearef = useRef()
+
+  const agregartarea = () => {
+    const tareas = tarearef.current.value.trim();
+
+    if (tareas === '') return;
+
+    tarearef.current.value = null
+
+    {/*id = uuid4(), tarea = tareas*/ }
+    setTodos((prevTodos) => {
+      const nuevatarea = {
+        id: uuid4, task: tareas, complete: false
+      }
+      return [...prevTodos, nuevatarea]
+    })
+  }
+
+  const cambiarestadotarea = (id) => {
+    const nuevoTodos = [...todos]
+
+    const todo = nuevoTodos.find((todo) => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(nuevoTodos)
+  }
+
+  const contadortareas = () => {
+    return todos.filter((todo) => !todo.complete).length
+  }
+
+  const Resumentareas = () => {
+    const cant = contadortareas()
+
+    if(cant === 0){
+      return(
+        <div className="alert alert-success mt-3">Felicidaddes has completado todas tus tareas OwO</div>
+      )
+    }
+    if(cant === 1){
+      return(
+        <div className="alert alert-info mt-3">Te queda solamente 1 tarea</div>
+      )
+    }
+    return(
+      <div className="alert alert-war mt-3">Te queda {cant} tarea pendientes</div>
+    )
+
+  }
+
   return (
     <Fragment>
       <h1>Lista de Tareas</h1>
+
+      {/* input para recibir los datos */}
+      <div className="input-group my-4">
+        <input className="form-control" placeholder="Ingrese una tarea" ref={tarearef}></input>
+        <button className="btn btn-success ms-3"><i className="bi bi-plus-circle-fill" onClick={agregartarea}></i></button>
+        <button className="btn btn-danger"><i className="bi bi-trash3-fill"></i></button>
+      </div>
+
       {/* Lista de tareas */}
       <ul className="list-group">
         {/* recorrer la lista */}
-        {todos.map((todo)=>(
-            <TodoItem todo={todo} key={todo.id}/>
+        {todos.map((todo) => (
+          <TodoItem todo={todo} key={todo.id} cambiarEstado={cambiarestadotarea}/>
         ))}
       </ul>
+        <Resumentareas />
     </Fragment>
   );
 }
